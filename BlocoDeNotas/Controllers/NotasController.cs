76c919cd80c21e3ee33d8675c724662e3f6a439a
Stephanie Gomes <gomess214@gmail.com>
2024-04-10 16:31:15 -0300

    Pagina de cadastro

---
+++ BlocoDeNotas/Controllers/NotasController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlocoDeNotas.Controllers
+{
+    public class NotasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
