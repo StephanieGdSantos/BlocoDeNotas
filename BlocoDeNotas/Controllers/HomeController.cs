957cc8d125757651045b06f149e7b73b4f6e5f3b
Stephanie Gomes <gomess214@gmail.com>
2024-04-16 16:30:07 -0300

    Adicionando função de erro

--- BlocoDeNotas/Controllers/HomeController.cs
+++ BlocoDeNotas/Controllers/HomeController.cs
@@ -8,5 +8,11 @@ namespace BlocoDeNotas.Controllers
         {
             return View();
         }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
